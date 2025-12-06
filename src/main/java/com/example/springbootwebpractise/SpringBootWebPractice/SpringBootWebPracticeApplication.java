e06c2617527188156b627175972cb628d783db71
Mayuri13 <mayuri.goswami2017@gmail.com>
2025-12-06 11:14:21 +0530

    added mvc architecture

---
+++ src/main/java/com/example/springbootwebpractise/SpringBootWebPractice/SpringBootWebPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebpractise.SpringBootWebPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootwebpractise/SpringBootWebPractice/SpringBootWebPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebpractise.SpringBootWebPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
